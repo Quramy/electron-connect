33284b025ad60a7b59bb9040beae83393ce3c695
Sreekanth Settipalli <ssettipalli@mbp-cgn.myden.com>
2016-05-18 21:53:07 -0700

    add tests using spectron to check application launch for examples

--- test/server.spec.js
+++ test/server.spec.js
@@ -1,5 +1,6 @@
 'use strict';
 
+var Application = require('spectron').Application;
 var assert = require('assert');
 var server = require('../lib/server');
 
@@ -52,5 +53,51 @@ describe('server', function () {
     });
   });
 
+  describe('#application launch: simple example', function () {
+    this.timeout(10000);
 
+      beforeEach(function () {
+        this.app = new Application({
+          path: __dirname + '/../node_modules/.bin/electron',
+          args: [__dirname + '/../example/simple']
+        });
+        return this.app.start();
+      });
+
+    afterEach(function () {
+      if (this.app && this.app.isRunning()) {
+        return this.app.stop();
+      }
+    });
+
+    it('shows an initial window', function () {
+      return this.app.client.getWindowCount().then(function (count) {
+        assert.equal(count, 1);
+      });
+    });
+  });
+
+  describe('#application launch: multiple-window example', function () {
+    this.timeout(10000);
+
+      beforeEach(function () {
+        this.app = new Application({
+          path: __dirname + '/../node_modules/.bin/electron',
+          args: [__dirname + '/../example/multiple-window']
+        });
+        return this.app.start();
+      });
+
+    afterEach(function () {
+      if (this.app && this.app.isRunning()) {
+        return this.app.stop();
+      }
+    });
+
+    it('shows two initial windows', function () {
+      return this.app.client.getWindowCount().then(function (count) {
+        assert.equal(count, 2);
+      });
+    });
+  });
 });
