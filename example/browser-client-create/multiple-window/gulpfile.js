8cfc0bb61a6a3541bc7c7dbfec02ce0feffbe963
Sreekanth S <s.sreekanth@outlook.com>
2016-06-09 21:54:08 -0700

    add example code to verify issue Quramy/electron-connect#45

---
+++ example/browser-client-create/multiple-window/gulpfile.js
@@ -0,0 +1,32 @@
+'use strict';
+
+var gulp = require('gulp');
+var electron = require('../../../').server.create({
+  verbose: true
+});
+
+gulp.task('serve', function () {
+
+  // Start browser process
+  electron.start();
+
+  // Restart browser process
+  gulp.watch('app.js', ['restart:browser']);
+
+  gulp.watch('*.html', ['reload:renderer']);
+
+});
+
+gulp.task('restart:browser', function(done) {
+  electron.restart();
+  done();
+});
+
+gulp.task('reload:renderer', function (done) {
+  // Reload renderer process
+  electron.reload();
+  done();
+});
+
+gulp.task('default', ['serve']);
+
