9cda23aff6bd48878d08496ee716e52b5829f285
Sreekanth S <s.sreekanth@outlook.com>
2016-06-10 20:02:33 -0700

    fix Quramy/electron-connect#32

---
+++ example/stop-on-close/gulpfile.js
@@ -0,0 +1,43 @@
+'use strict';
+
+var gulp = require('gulp');
+var electron = require('../../').server.create({
+  stopOnClose: true,
+  verbose: true
+});
+
+var callback = function(electronProcState) {
+  console.log('electron process state: ' + electronProcState);
+  if (electronProcState == 'stopped') {
+    process.exit();
+  }
+};
+
+gulp.task('serve', function () {
+
+  // Start browser process
+  electron.start(callback);
+
+  // Restart browser process
+  gulp.watch('app.js', ['restart:browser']);
+
+  gulp.watch('*.html', ['reload:renderer']);
+
+});
+
+gulp.task('restart:browser', function(done) {
+  electron.restart(callback);
+  done();
+});
+
+gulp.task('reload:renderer', function (done) {
+  // Reload renderer process
+  electron.reload(callback);
+  setTimeout(function () {
+    electron.broadcast('myNotification');
+    done();
+  });
+});
+
+gulp.task('default', ['serve']);
+
